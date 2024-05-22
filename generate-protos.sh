35af6ce893e0c8a4468af142eb797ae87d815969
Lucy Sweet <intUnderflow@noreply.users.github.com>
2024-05-22 02:09:16 +0200

    Add GRPC

--- generate-protos.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-protoc --proto_path=proto --go_out=proto --go_opt=paths=source_relative proto/metalinfraconfig.proto
---
+++ proto/generate-protos.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+protoc --proto_path=. --go_out=. --go-grpc_out=. --go_opt=paths=source_relative --go-grpc_opt=paths=source_relative metalinfraconfig.proto
